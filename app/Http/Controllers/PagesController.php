78f735a91ced70d9328c5b1f80c1b41a43291cfd
hasnanab <47822253+hasnanab@users.noreply.github.com>
2020-04-07 16:18:32 +0700

    Add files via upload

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -3,8 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class PagesController extends Controller
 {
-    //
+    public function index(){
+        // return view('produk');
+        $produk = DB::table('produk')->get();
+        return view('produk', ['produk'=>$produk]);
+    }
 }
