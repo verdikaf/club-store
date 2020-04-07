cf666e0f4ce747d1b511a8627e94a93776d1d6d3
neviliadita17 <61975618+neviliadita17@users.noreply.github.com>
2020-04-08 00:32:06 +0700

    Update produk_edit.blade.php

--- resources/views/produk_edit.blade.php
+++ resources/views/produk_edit.blade.php
@@ -31,18 +31,17 @@
     @foreach($produk as $p)
     <form action="/produk/edit/save" method="post">
     {{ csrf_field() }}
-        <div class="container-fluid" style="padding-top: 15px;">
-            <div class="row">
+        <div class="container" style="padding-top: 15px;">
+            <div class="row" style=>
             <div class="col-md-12">
             <!-- general form elements -->
-            <div class="card card-primary">
-              <div class="card-header">
+            <div class="card">
+            <div class="card-header">
                 <h3 class="card-title">Update Produk</h3>
+                <button type="submit" class="btn btn-warning">Update </button>
               </div>
-              <!-- /.card-header -->
-              <!-- form start -->
-              <form role="form">
-                <div class="card-body">
+            
+              <div class="card-body">
                 <div class="form-group">
                     <input type="hidden" name="id" value="{{$p->id}}">
                     <label for="nama_produk">Nama</label>
@@ -70,12 +69,10 @@
                             @endforeach
                   </div>
                 </div>
-                <!-- /.card-body -->
-
+            
                 <div class="card-footer">
-                  <button type="submit" class="btn btn-primary">Insert</button>
+                  
                 </div>
-              </form>
             </div>
             <!-- /.card -->
             </div>
