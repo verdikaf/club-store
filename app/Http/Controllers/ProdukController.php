a0729c3c812da7c9bcc9d50449ffe6f1e6df9684
neviliadita17 <61975618+neviliadita17@users.noreply.github.com>
2020-04-14 02:16:20 +0700

    Update ProdukController.php

--- app/Http/Controllers/ProdukController.php
+++ app/Http/Controllers/ProdukController.php
@@ -47,46 +47,25 @@ class ProdukController extends Controller
 
     public function produkEdit($id)
     {
-
-        // $data['produk'] = DB::table("SELECT * FROM produk WHERE id=?", [$id]);
-        // $data['kategori'] = DB::select("SELECT * FROM kategori");
-        // $data['supplier'] = DB::select("SELECT * FROM supplier");
         $produk = DB::table('produk')->where('id',$id)->get();
         $supplier = DB::table('supplier')->get();
         $kategori = DB::table('kategori')->get();
         return view('produk_edit',['produk' => $produk, 'kategori' => $kategori, 'supplier' => $supplier]);
-        // return view('produk_edit',$data);
     }
 
     public function produkEditSave(Request $request)
     {
         $method = $request->method();
             if($method == "POST"){
-                if ($request->file('file') == null){
-                DB::update("UPDATE produk SET nama=?  WHERE id = ?", [
-                    $request->input('nama'),
-                    $request->input('stok'),
-                    $request->input('harga'),
-                    $directory."/".$file->getClientOriginalName(),
-                    $request->input('kategori_id'),
-                    $request->input('supplier_id')
-                    ]);
-                }else{
-                    $directory = 'assets/produk';
-                    $file      = $request->file('foto');
-                    $file->move($directory, $file->getClientOriginalName());
-
-                    DB::update("UPDATE produk SET nama=?, stok=?, harga=?, foto=?, kategori_id=?, supplier_id=? WHERE id = ?", [
+                    DB::update("UPDATE produk SET nama=?, stok=?, harga=?, kategori_id=?, supplier_id=? WHERE id = ?", [
                         $request->input('nama'),
                         $request->input('stok'),
                         $request->input('harga'),
-                        $directory."/".$file->getClientOriginalName(),
                         $request->input('kategori_id'),
-                        $request->input('supplier_id')
+                        $request->input('supplier_id'),
+                        $request->input('id')
                         ]);
-                }
                 return redirect('/produk');
-
             }else{
                 return redirect('/produk');
         }
