05b2b540f4411398edf1fb65cb45071eb67b488c
alwanhafidzin <61824015+alwanhafidzin@users.noreply.github.com>
2020-04-24 19:56:40 +0700

    Update login_employee.blade.php

--- resources/views/login_employee.blade.php
+++ resources/views/login_employee.blade.php
@@ -1,57 +1,73 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
+    <!-- Required meta tags-->
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="Colorlib Templates">
+    <meta name="author" content="Colorlib">
+    <meta name="keywords" content="Colorlib Templates">
+
+    <!-- Title Page-->
     <title>{{$title}}</title>
-  
 
+    <!-- Icons font CSS-->
+    <link href="{{url('/assets/library/loginlibrary/material-design-iconic-font.min.css')}}" rel="stylesheet" media="all">
+    <link href="{{url('assets/library/loginlibrary/font-awesome.min.css')}}" rel="stylesheet" media="all">
+    <!-- Font special for pages-->
+    <link href="https://fonts.googleapis.com/css?family=Poppins:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 
-    <link rel="stylesheet" href="{{url('/assets/library/loginlibrary/bootstrap.min.css')}}">
-    <link rel="stylesheet" href="{{url('/assets/library/loginlibrary/style.css')}}">
-    <script type="text/javascript" src="{{'/assets/library/loginlibrary/bootstrap.min.js' }}"></script>
-    <script type="text/javascript" src="{{'/assets/library/loginlibrary/jquery.min.js' }}"></script>
-  
-    
+    <!-- Vendor CSS-->
+    <link href="{{url('assets/library/loginlibrary/select2.min.css')}}" rel="stylesheet" media="all">
+    <!-- <link href="vendor/datepicker/daterangepicker.css" rel="stylesheet" media="all"> -->
+
+    <!-- Main CSS-->
+    <link href="{{url('assets/library/loginlibrary/main.css')}}" rel="stylesheet" media="all">
 </head>
 
 <body>
+@include('flash-message')
 
-    <div id="login">
-    @include('flash-message')
-
-    @yield('content')
-
-        <h3 class="text-center text-white pt-5">CLUB STORE LOGIN</h3>
-        <div class="container">
-            <div id="login-row" class="row justify-content-center align-items-center">
-                <div id="login-column" class="col-md-6">
-                    <div id="login-box" class="col-md-12">
-                        <form id="login-form" class="form" action="{{url('/login/employee/action')}}" method="post">
-                        @csrf
-                            <h3 class="text-center text-info">Login</h3>
-                            <div class="form-group">
-                                <label for="email" class="text-info">Email:</label><br>
-                                <input type="mail" name="email" id="email" class="form-control" placeholder="Masukkan Email disini" required>
+@yield('content')
+    <div class="page-wrapper bg-gra-03 p-t-130 p-b-100 font-poppins">
+        <div class="wrapper wrapper--w680">
+            <div class="card card-4">
+                <div class="card-body">
+                <h2 class="title">Club Store Login</h2>
+                    <form method="POST" action="{{url('/login/employee/action')}}">
+                    @csrf
+						<div class="input-group">
+                            <div class="input-group">
+                                    <label class="label">Email</label>
+                                    <input class="input--style-4" id="email" type="email" name="email">
                             </div>
-                            <div class="form-group">
-                                <label for="password" class="text-info">Password:</label><br>
-                                <input type="password" name="password" id="password" class="form-control" placeholder="Masukkan password disini" requaired>
+                        </div>
+                        <div class="input-group">
+                            <div class="input-group">
+                                    <label class="label">Password</label>
+                                    <input class="input--style-4"id="password" type="password" name="password">
                             </div>
-                            <div class="form-group">
-                                <label for="remember-me" class="text-info"><span>Remember me</span> <span><input id="remember-me" name="remember-me" type="checkbox"></span></label><br>
-                                <input type="submit" name="submit" class="btn btn-info btn-md" value="submit">
-                            </div>
-                            <div id="register-link" class="text-right">
-                                <a href="{{url('/register/employee')}}" class="text-info">Register here</a>
-                            </div>
-                        </form>
-                    </div>
+                        </div>
+                        <div class="p-t-15">
+                            <button class="btn btn--radius-2 btn--blue" type="submit">Login</button>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
     </div>
-</body>
 
+    <!-- Jquery JS-->
+    <script src="{{url('assets/library/loginlibrary/jquery.min.js')}}"></script>
+    <!-- Vendor JS-->
+    <script src="{{url('loginlibrary/select2.min.js')}}"></script>
+    <!-- <script src="vendor/datepicker/moment.min.js"></script>
+    <script src="vendor/datepicker/daterangepicker.js"></script> -->
+
+    <!-- Main JS-->
+    <script src="{{url('assets/library/loginlibrary/global.js')}}"></script>
+
+</body>
 </html>
+<!-- end document-->
